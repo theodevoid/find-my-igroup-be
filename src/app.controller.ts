a27cc59f313d6aa71ca1f4a6f244636d3308afc0
theodevoid <voidfnc9@gmail.com>
2024-04-03 01:12:37 +0700

    hello world

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from './auth/auth.guard';
-import { UserID } from './user/user.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @UseGuards(AuthGuard)
-  getHello(@UserID() userId: number): number {
-    return userId;
+  getHello() {
+    return 'Hello World!';
   }
 }
